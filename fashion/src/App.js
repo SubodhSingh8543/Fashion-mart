46e1215163384b362f7423b6b2d77b12dfe24d5e
SubodhSingh8543 <subodhsingh8543@gmail.com>
2023-01-18 21:13:31 +0530

    doneEval

--- fashion/src/App.js
+++ fashion/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import BelowNavbar from './Components/BelowNavbar';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
+import { Divider } from "@chakra-ui/react";
+import FooterBelow from './Components/FooterBelow';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Divider/>
+      <BelowNavbar/>
       
+      <Footer/>
+      <FooterBelow/>
     </div>
   );
 }
